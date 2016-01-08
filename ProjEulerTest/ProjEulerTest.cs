e7969a1055cc4e3d74096f2230ea94b45c03d2ae
f3c288281a819b74 <f3c288281a819b74@users.noreply.github.com>
2016-01-08 21:18:23 +0100

    Init

---
+++ ProjEulerTest/ProjEulerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ProjEuler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjEulerTest
+{
+    [TestClass]
+    public class ProjEulerTest
+    {
+        [TestMethod]
+        public void Problem001Test()
+        {
+            ProjEuler.ProjEuler.Problem001();
+        }
+    }
+}
